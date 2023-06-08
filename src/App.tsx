8f003b7ba8351fbdb2226b09820142113938cdfc
“Isabella <laschi.isabella@gmail.com>
2023-06-08 09:33:54 -0300

    removed unused icons

--- src/App.tsx
+++ src/App.tsx
@@ -15,40 +15,40 @@ export default function App() {
   }, [])
 
   return (
-    <div className="bg-gray-900 max-w-screen min-h-screen flex justify-center items-start font-roboto">
+    <div className="flex items-start justify-center min-h-screen bg-gray-900 max-w-screen font-roboto">
       {/* wrapper */}
-     <div className="flex gap-10 w-full max-w-screen-lg mt-16 mx-5">
+     <div className="flex w-full max-w-screen-lg gap-10 mx-5 mt-16">
       {/* left */}
       <div className="max-w-xs">
-        <div className="text-white flex flex-col items-center">
+        <div className="flex flex-col items-center text-white">
           <img src="/logo.png" alt="logo" className="w-1/2 "/>
-          <span className="font-murecho uppercase text-3xl mt-5 mb-1">Isabella Laschi</span>
-          <span className="font-major text-xl">full<span className="text-gray-200"> stack </span>developer</span>
+          <span className="mt-5 mb-1 text-3xl uppercase font-murecho">Isabella Laschi</span>
+          <span className="text-xl font-major">full<span className="text-gray-200"> stack </span>developer</span>
         </div>
         <About />
       </div>
       {/* right */}
       <div className="w-full">
-        <div className="flex justify-evenly text-center gap-2 ">
+        <div className="flex gap-2 text-center justify-evenly ">
           <div 
-            className="bg-yellow-200 w-full ml-3 rounded-t-md py-1 cursor-pointer"
+            className="w-full py-1 ml-3 bg-yellow-200 cursor-pointer rounded-t-md"
             onClick={() => setTab('skills')}
           >{t('tabs.tab1')}</div>
           <div 
-            className="bg-purple-700 w-full mr-3 rounded-t-md py-1 cursor-pointer"
+            className="w-full py-1 mr-3 bg-purple-700 cursor-pointer rounded-t-md"
             onClick={() => setTab('projects')}
           >{t('tabs.tab2')}</div>
         </div>
 
         { tab == 'skills' && 
-          <div className="bg-yellow-200 rounded-md p-8 mb-16">
+          <div className="p-8 mb-16 bg-yellow-200 rounded-md">
             <Skill />
             <Skill />
             <Skill />
           </div>
          }
         { tab == 'projects' && 
-          <div className="bg-purple-700 rounded-md p-12 mb-16 flex flex-col gap-10">
+          <div className="flex flex-col gap-10 p-12 mb-16 bg-purple-700 rounded-md">
             <Project />
             <Project />
             <Project />
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import { MapPin, At, GithubLogo, LinkedinLogo, CaretCircleDoubleRight, Globe, MoonStars, Sun } from "@phosphor-icons/react";
+import { MapPin, At, GithubLogo, LinkedinLogo, CaretCircleDoubleRight} from "@phosphor-icons/react";
 import i18next from "i18next";
 import { useTranslation } from "react-i18next";
 
@@ -16,10 +16,10 @@ export default function About(){
   }
 
   return(
-    <div className="bg-gray-500 my-5 p-8 text-gray-100 rounded-md transition">
+    <div className="p-8 my-5 text-gray-100 transition bg-gray-500 rounded-md">
       <div className="flex justify-end">
         {/* language */}
-        <div className="flex gap-2 items-center">
+        <div className="flex items-center gap-2">
           <p className="text-sm font-bold text-gray-100">PT</p>
           <label className="relative inline-flex items-center">
             <input type="checkbox" value="" className="sr-only peer" />
@@ -33,16 +33,16 @@ export default function About(){
         <h1 className="text-2xl text-black"><span className="font-bold">{t('about.titleFirst')}</span> {t('about.titleSecond')}</h1>
         <p className="font-light">{t('about.description')}</p>
       </div>
-      <div className="text-base my-6 ml-1">
-        <span className="flex gap-3 items-center"><MapPin size={20} color="#000" weight="fill" /><span>{t('about.location')}</span></span>
-        <span className="flex gap-3 items-center cursor-pointer transition hover:text-black"><At size={20} color="#000"/><a href="mailto:laschi.isabella@gmail.com">laschi.isabella@gmail.com</a></span>
+      <div className="my-6 ml-1 text-base">
+        <span className="flex items-center gap-3"><MapPin size={20} color="#000" weight="fill" /><span>{t('about.location')}</span></span>
+        <span className="flex items-center gap-3 transition cursor-pointer hover:text-black"><At size={20} color="#000"/><a href="mailto:laschi.isabella@gmail.com">laschi.isabella@gmail.com</a></span>
       </div>
       <div className="flex justify-between">
         <div className="flex gap-1 text-gray-900">
-          <a href="https://github.com/laschisabella" target="_blank"><GithubLogo size={32} weight="fill" className="rounded cursor-pointer transition hover:text-white hover:bg-gray-900"/></a>
-          <a href="https://www.linkedin.com/in/isabella-laschi/" target="_blank"><LinkedinLogo size={32} weight="fill" className="rounded cursor-pointer transition hover:text-white hover:bg-gray-900"/></a>
+          <a href="https://github.com/laschisabella" target="_blank"><GithubLogo size={32} weight="fill" className="transition rounded cursor-pointer hover:text-white hover:bg-gray-900"/></a>
+          <a href="https://www.linkedin.com/in/isabella-laschi/" target="_blank"><LinkedinLogo size={32} weight="fill" className="transition rounded cursor-pointer hover:text-white hover:bg-gray-900"/></a>
         </div>
-        <div className="flex uppercase gap-2 items-center font-bold text-gray-100 bg-gray-900 px-4 rounded-md text-sm transition cursor-pointer hover:bg-gray-700">
+        <div className="flex items-center gap-2 px-4 text-sm font-bold text-gray-100 uppercase transition bg-gray-900 rounded-md cursor-pointer hover:bg-gray-700">
           <CaretCircleDoubleRight weight="fill" size={20} />
           <p className="block mt-0.5">{t('about.button')}</p>
         </div>
