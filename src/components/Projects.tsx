d204446b4c643c3a7a0a1e1a1f5fcb83ff32e308
“Isabella <laschi.isabella@gmail.com>
2023-07-19 09:01:40 -0300

    commit

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,14 +6,35 @@ import {
   AccordionPanel,
   AccordionIcon,
 } from '@chakra-ui/react'
-import { t } from "i18next";
+import { DefaultTFuncReturn, t } from "i18next";
+
+interface ProjectI {
+  name: DefaultTFuncReturn
+  description: {
+    desc1: DefaultTFuncReturn,
+    desc2: DefaultTFuncReturn,
+    ul: [DefaultTFuncReturn, DefaultTFuncReturn][]
+  }
+  urlRepository: string
+  urlProduction?: string
+  tech: string[]
+}
 
 export default function Project(){
 
-  const projects = [
+  const projects:ProjectI[] = [
     {
       name: t('projects.project1.name'),
-      description: t('projects.project1.description'),
+      description: {
+        desc1: t('projects.project1.description1'),
+        desc2: t('projects.project1.description2'),
+        ul: [
+          [t('projects.project1.ul.li1.title'), t('projects.project1.ul.li1.description')],
+          [t('projects.project1.ul.li2.title'), t('projects.project1.ul.li2.description')],
+          [t('projects.project1.ul.li3.title'), t('projects.project1.ul.li3.description')],
+          [t('projects.project1.ul.li4.title'), t('projects.project1.ul.li4.description')],
+        ]
+      },
       urlRepository: 'https://github.com/laschisabella/tech-blog',
       urlProduction: 'https://tech-blog-laschisabella.vercel.app/',
       tech: [
@@ -22,31 +43,43 @@ export default function Project(){
         'https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white',
       ],
     },
-    {
-      name: t('projects.project2.name'),
-      description: t('projects.project2.description'),
-      urlRepository: 'https://github.com/laschisabella/nodejs-fundamentals/',
-      tech: [
-        'https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white'
-      ],
-    },
+    // {
+    //   name: t('projects.project2.name'),
+    //   description: {
+    //     desc1: t('projects.project2.description1'),
+    //     desc2: t('projects.project2.description2'),
+    //     ul: [
+    //       [t('projects.project2.ul.li1.title'), t('projects.project2.ul.li1.description')],
+    //       [t('projects.project2.ul.li2.title'), t('projects.project2.ul.li2.description')],
+    //       [t('projects.project2.ul.li3.title'), t('projects.project2.ul.li3.description')],
+    //       [t('projects.project2.ul.li4.title'), t('projects.project2.ul.li4.description')],
+    //     ]
+    //   },
+    //   urlRepository: 'https://github.com/laschisabella/nodejs-fundamentals/',
+    //   tech: [
+    //     'https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white'
+    //   ],
+    // },
   ]
 
   return (
-    <div className="flex flex-col p-10 bg-yellow-200 rounded-md gap-7">
-      <div className="mb-3">
-        <h1 className="mb-3 text-2xl font-murecho">{t('projects.title')}</h1>
-        <p className="">{t('projects.intro')}</p>
+    <div className="flex flex-col w-full bg-purple-600 rounded-md p-14 gap-7">
+      <div className="">
+        <h1 className="text-3xl text-purple-800 font-murecho">
+          <span className="text-purple-700">&lt;</span>
+          <span className="px-1">{t('projects.title')}</span>
+          <span className="text-purple-700">/&gt;</span>
+        </h1>
       </div>
-      <Accordion borderColor="#b1a358" >
+      <Accordion borderColor="transparent" className="">
         {
           projects.map(project => (
-          <AccordionItem>
-            <AccordionButton className="flex justify-between">
-              <div className="flex flex-col items-center justify-between w-full mr-5 sm:flex-row">
-                <h1 className="py-3 text-xl text-black font-murecho">{project.name}</h1>
+          <AccordionItem className="my-1">
+            <AccordionButton className="flex justify-between gap-2 bg-purple-900 rounded-lg">
+              <div className="flex items-center justify-between w-full gap-12">
+                <h1 className="py-3 pl-2 text-gray-100 text-md font-murecho hover:bg-yellow-700">{project.name}</h1>
                 {/* tech */}
-                <div className="flex flex-wrap justify-center gap-1 my-1">
+                <div className="flex flex-wrap justify-center gap-1">
                   {
                     project.tech.map(image => (
                       <img 
@@ -57,34 +90,51 @@ export default function Project(){
                   }
                 </div>
               </div>
-              <AccordionIcon />
+              <AccordionIcon className="text-gray-100"/>
             </AccordionButton>
-            <AccordionPanel className="flex flex-col">
+            <AccordionPanel className="flex flex-col mt-1 text-gray-100 bg-purple-700 rounded-lg">
               {/* description */}
-              <p>{t('projects.project1.description')}</p>
-
-              {/* buttons */}
-              <div className="flex gap-3 my-3 w-min">
-              {
-                  project.urlProduction && (
-                    <a href={project.urlProduction} role="button" target="_blank" >
-                      <div className="flex gap-1 text-sm uppercase transition hover:text-purple-900 font-murecho">
-                        <CaretDoubleRight weight="bold" size={20} className="text-purple-500 "/>
-                        <p>{t('projects.buttons.production')}</p>
-                      </div>
-                    </a>
+              <div className="pr-5 m-5 overflow-y-auto h-52">
+                <p>{project.description.desc1}</p>
+                {
+                  project.description.ul && (
+                    <ul className="list-none">
+                      {
+                        project.description.ul.map(li => (
+                          <li className="py-3">
+                            <strong className="font-black text-purple-800">{li[0]}: </strong>
+                            <span>{li[1]}</span>
+                          </li>
+                        ))
+                      }
+                    </ul>
                   )
                 }
+                {
+                  project.description.desc2 && (
+                    <p>{project.description.desc2}</p>
+                  )
+                }
+              </div>
+              {/* buttons */}
+              <div className="flex gap-3 mx-auto justify-content w-min">
+              {
+                project.urlProduction && (
+                  <a href={project.urlProduction} role="button" target="_blank" >
+                    <div className="flex gap-1 text-sm text-black uppercase transition hover:text-purple-100 font-murecho">
+                      <CaretDoubleRight weight="bold" size={20} className="text-purple-600"/>
+                      <p>{t('projects.buttons.production')}</p>
+                    </div>
+                  </a>
+                )
+              }
                 <a href={project.urlRepository} role="button" target="_blank">
-                  <div className="flex gap-1 text-sm uppercase transition hover:text-purple-900 font-murecho">
-                    <CaretDoubleRight weight="bold" size={20} className="text-purple-500 "/> 
+                  <div className="flex gap-1 text-sm text-black uppercase transition hover:text-purple-100 font-murecho">
+                    <CaretDoubleRight weight="bold" size={20} className="text-purple-600 "/> 
                     <p className="mb-0.5">{t('projects.buttons.repository')}</p>
                   </div>
                 </a>
-                
               </div>
-
-              
             </AccordionPanel>
           </AccordionItem>
           ))
--- src/translations/portuguese.ts
+++ src/translations/portuguese.ts
@@ -2,21 +2,38 @@ export const portuguese = {
   about: {
     titleFirst: "Sobre",
     titleSecond: "mim",
-    description: "Desenvolvedora Full Stack especializada no ecossistema JavaScript, com mais de 3 anos de experiência em ferramentas de desenvolvimento web completo e aplicações centradas na experiência do usuário.",
+    description: "Desenvolvedora Full Stack especializada no ecossistema JavaScript, com mais de 3 anos de experiência em  desenvolvimento web e aplicações centradas na experiência do usuário.",
     location: "São Paulo, Brasil",
     button: "currículo",
   },
   projects: {
-    title: 'Olá, bem-vindo(a) à minha galeria de projetos.',
-    intro: 'Confira aqui alguns trabalhos que foram criados com o objetivo de explorar as últimas tendências em ferramentas e metodologias de desenvolvimento web, além de aprimorar habilidades técnicas e criativas. Espero que você aprecie os projetos apresentados, fique à vontade para conhecer mais e entrar em contato caso tenha alguma pergunta ou oportunidade de colaboração.',
+    title: 'Projetos',
     buttons: {
       repository: 'repositório',
       production: 'produção',
     },
     project1: {
-      
       name: "Laschisa's Tech Blog",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad necessitatibus libero consequuntur quod illum ipsum quia omnis dolor aperiam. Quisquam quod eius distinctio minima atque, inventore fugit illum? Neque, fuga.',
+      description1: 'O objetivo deste projeto é explorar uma variedade de tecnologias relevantes, ao criar um projeto completo e funcional. Os recursos utilizados foram:',
+      ul: {
+        li1: {
+          title: 'Next.js 13.4',
+          description: 'Aproveitando a nova versão do Next.js, foi possível utilizar a tecnologia App Router para criar componentes de servidor e cliente. Essa abordagem permitiu fazer chamadas diretas à API a partir do componente servidor, eliminando a necessidade de utilizar as funções getStaticProps e getServiceSideProps. Isso resultou em melhorias significativas no desempenho e otimização para mecanismos de busca (SEO).',
+        },
+        li2: {
+          title: 'TailwindCSS',
+          description: 'O TailwindCSS foi adotado para desenvolver uma interface simples e eficiente. Essa biblioteca oferece uma ampla variedade de estilos pré-definidos, que podem ser facilmente aplicados aos componentes, poupando tempo no processo de desenvolvimento.',
+        },
+        li3: {
+          title: 'Mongoose',
+          description: 'O Mongoose foi integrado ao projeto para criar uma API que se comunica com um banco de dados MongoDB, permitindo a realização de operações CRUD. Essa biblioteca simplifica a interação com o banco de dados, fornecendo uma camada de abstração conveniente.',
+        },
+        li4: {
+          title: 'Next Auth',
+          description: 'O Next Auth foi utilizado para possibilitar o cadastro de usuários através de email e senha, além da autenticação utilizando o Google Authenticator. Essa biblioteca simplifica o processo de autenticação, fornecendo recursos prontos para uso, eliminando a necessidade de desenvolver tudo do zero.',
+        },
+      },
+      description2: 'Essas tecnologias foram escolhidas estrategicamente para otimizar o desenvolvimento e proporcionar uma experiência de usuário aprimorada. Ao explorar o Next.js, TailwindCSS, Mongoose e Next Auth, foi possível aperfeiçoar habilidades técnicas em ferramentas amplamente requisitas no mercado profissional.'
     },
     project2: {
       name: "Fundamentos de Node.js",
